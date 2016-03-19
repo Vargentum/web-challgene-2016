20d89219f9a0b805eae4dda79e941f20fcdd03b8
Vargentum <vargentum@gmail.com>
2016-03-19 13:49:42 +0200

    construct table

--- gant-diagram/diagramBuilder.js
+++ gant-diagram/diagramBuilder.js
@@ -2,6 +2,14 @@
 
 var DiagramBuilder
 (function (DiagramBuilder) {
+  var datePattern = /\d{4}\-\d{2}\-\d{2}/
+  var SCALE_TYPES = {
+    hour: 60 * 60 * 100,
+    day: 24 * 60 * 60 * 100,
+    week: 7 * 24 * 60 * 60 * 100,
+    month: 30 * 24 * 60 * 60 * 100,
+  }
+
 
   function makeRequest(file, onsuccess) {
     var xhr = new XMLHttpRequest()
@@ -14,10 +22,67 @@ var DiagramBuilder
     }
   }
 
+  function dateToInt (s) { return new Date(s).getTime()}
+
+  function getLowest(d1, d2) {
+    return dateToInt(d1) < dateToInt(d2) ? d1 : d2
+  }
+  function getLargest(d1, d2) {
+    return dateToInt(d1) > dateToInt(d2) ? d1 : d2
+  }
+
+
+  function getTotalTasksTiming (data) {
+    return data.reduce(function(p, n) {
+      return {
+        start_date: getLowest(n.start_date, p.start_date),
+        end_date:   getLargest(n.end_date, p.end_date)
+      }
+    })
+  }
+
+  function createTimingPanel(data, scale) {
+    var scale = 'month'
+    var timing = getTotalTasksTiming(data)
+    var panel = []
+    for (var i = dateToInt(timing.start_date);
+             i <= dateToInt(timing.end_date);
+             i += SCALE_TYPES[scale]) {
+      (function(i) {
+        panel.push(new Date(i).toString())
+      })(i)
+    }
+    return panel
+  }
+
+
+  function buildRow(data, initField, cb) {
+    let row = [initField].concat(createTimingPanel(data))
+    row.forEach(cb)
+  }
+
+
   function makeDiagramFrom(data) {
-    var root = document.createElement('pre')
-    root.innerText = JSON.stringify(data)
-    return root
+    var table = document.createElement('table')
+    var head = table.createTHead()
+    var headRow = head.insertRow()
+
+    buildRow(data, 'name', function(text) {
+      var cell = headRow.insertCell()
+      cell.innerText = text
+    })
+
+    data.forEach(function(entry) {
+      var bodyRow = table.insertRow()
+      buildRow(data, entry.text, function(text, idx) {
+        var cell = bodyRow.insertCell()
+        if (idx === 0) {
+          cell.innerText = text
+        }
+      })
+    })
+
+    return table
   }
 
 
