201422a4ba0a54c4b2383594d4e89537191d7cf4
Vargentum <vargentum@gmail.com>
2016-03-20 14:50:16 +0200

    refactor to separate groups & chart builder

--- gant-diagram/app.js
+++ gant-diagram/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
+var TaskModel = {
+  Duration: 'number',
+  Id: 'string',
+  Name: 'string',
+  PersentDone: 'number',
+  StartDate: 'date',
+  Tasks: 'array',
+  expanded: 'bool'
+}
+
+
+
 var Utils = (function() {
 
   var Utils = {}
@@ -17,8 +29,8 @@ var Utils = (function() {
   }
 
   Utils.template = function(str, data) {
-    Object.keys(data).forEach(function(key, i, data) {
-      str = str.replace(new RegExp("\$\{\("+ data[key] +")\}", '$1'))
+    Object.keys(data).forEach(function(key) {
+      str = str.replace(new RegExp("\\${"+key+"}", 'g'), data[key])
     })
     return str
   }
@@ -29,12 +41,10 @@ var Utils = (function() {
 
 
 /*-------------------------------------------
-DiagramBuilder
+ChartBuilder
 -------------------------------------------*/
+var ChartBuilder = (function () {
 
-
-var DiagramBuilder = (function () {
-  
   var SCALE_TYPES = {
     hour: 60 * 60 * 1000,
     day: 24 * 60 * 60 * 1000,
@@ -42,17 +52,7 @@ var DiagramBuilder = (function () {
     month: 30 * 24 * 60 * 60 * 1000,
   }
   var DEFAULT_SCALE_TYPE = 'day'
-  
-  function makeRequest(file, onsuccess) {
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', file)
-    xhr.send()
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== 4) return //loading handling
-      if (xhr.status !== 200) return //error handling
-      onsuccess(JSON.parse(xhr.responseText))
-    }
-  }
+
 
   function dateToInt (s) { return new Date(s).getTime()}
 
@@ -101,9 +101,9 @@ var DiagramBuilder = (function () {
         var cell = bodyRow.insertCell()
         if (idx === 0) {
           cell.innerText = date
-        } 
+        }
         else if(
-          dateToInt(date) >= dateToInt(entry.start_date) && 
+          dateToInt(date) >= dateToInt(entry.start_date) &&
           dateToInt(date) <= dateToInt(entry.end_date)
         ) {
           cell.classList.add('is-active')
@@ -119,26 +119,25 @@ var DiagramBuilder = (function () {
   }
 
 
-  function DiagramBuilder(config) {
+  function ChartBuilder(config) {
     this.config = config
     this.config.scale = config.scale || DEFAULT_SCALE_TYPE
   }
 
-  DiagramBuilder.prototype.init = function(){
+  ChartBuilder.prototype.render = function(){
     var handleUpdate = function(data) {
-      debugger
       this.config.elem.innerHTML = ""
       this.config.elem.appendChild(makeDiagramFrom(data, this.config.scale))
     }
     makeRequest(this.config.file, handleUpdate.bind(this))
   };
 
-  DiagramBuilder.prototype.update = function(newConfig){
+  ChartBuilder.prototype.update = function(newConfig){
     this.config = Utils.objectAssign(this.config, newConfig)
-    this.init(this.config) 
+    this.init(this.config)
   }
 
-  return DiagramBuilder
+  return ChartBuilder
 })()
 
 
@@ -146,25 +145,92 @@ var DiagramBuilder = (function () {
 /*------------------------------------------------
   GroupsBuilder
 ------------------------------------------------*/
+var GroupsBuilder = (function () {
 
-
-var GroupsBuilder = (function (GroupsBuilder) {
-
-  function r_list(data) {
-    return Object.keys(data).reduce(function(p, key, data) {
-
-      return p += Utils.template(`<li>${callback}</li>`)
-    }, Utils.template(`<ul>${data}</ul>`, data))
+  function r_tasks(data) {
+    var list = document.createElement('ul')
+    var tasks = []
+    if (data.Tasks) {
+      Array.isArray(data.Tasks.Task) 
+        ? tasks = data.Tasks.Task
+        : tasks = [data.Tasks.Task]
+    }
+    tasks.forEach(function(task) {
+      var item = document.createElement('li')
+      item.innerText = task.Name
+      if (task.Tasks) item.appendChild(r_tasks(task))
+      list.appendChild(item)
+    })
+    return list
   }
 
-  function GroupsBuilder(json) {
-    this.groups = JSON.parse(json)
-    this.list = document.createElement('ul')
+
+  function GroupsBuilder(data, config) {
+    this.data = data
+    this.config = config
+    this.list = r_tasks(this.data)
   }
 
-  GroupsBuilder.prototype.init = function init(){
-    this.list.innerHTML = r_list(this.groups)
+  GroupsBuilder.prototype.mountTo = function mountTo(point) {
+    point.appendChild(this.list)
   }
 
   return GroupsBuilder
-})(GroupsBuilder || (GroupsBuilder = {}))
+})()
+
+
+
+
+
+/*------------------------------------------
+  App
+------------------------------------------*/
+var App = (function () {
+
+  function getData(file, onsuccess) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', file)
+    xhr.send()
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== 4) return //loading handling
+      if (xhr.status !== 200) return //error handling
+      onsuccess(JSON.parse(xhr.responseText))
+    }
+  }
+
+
+  function App (config) {
+    this.config = config
+    this.init()
+  }
+
+  App.prototype.init = function() {
+    var groups = document.createElement('div')
+      , chart = document.createElement('div')
+
+    function initComponents(data) {
+      this.GroupsBuilder = new GroupsBuilder(data, this.config)
+      // this.ChartBuilder = new ChartBuilder(data, this.config)
+      this.GroupsBuilder.mountTo(groups)
+      // this.ChartBuilder.mountTo(chart)
+
+      this.config.elem.appendChild(groups)
+      // this.config.elem.appendChild(chart)
+    }
+    getData(this.config.file, initComponents.bind(this))
+
+
+  }
+
+  App.prototype.update = function(newConfig) {
+    this.config = Utils.objectAssign(this.config, newConfig)
+    this.init()
+  }
+
+  return App
+})()
+
+
+
+
+
